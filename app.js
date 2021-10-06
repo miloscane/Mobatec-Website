9999c710ece6dcd48b01d1cfcaac2e4570f104aa
Milos <76661053+miloscane@users.noreply.github.com>
2021-10-06 16:10:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -794,7 +794,7 @@ server.post('/modeller-login',function(req,res){
 					}else{
 						if(result.length>0){
 							if(result[0].password==password){
-								res.redirect(result[0].url+"/vnc_lite.html?password="+result[0].password+"&resize=remote");
+								res.redirect(result[0].url+"/vnc_lite.html?password="+result[0].password);
 							}else{
 								res.render('message',{
 									pageInfo: fetchPageInfo('message',''),
